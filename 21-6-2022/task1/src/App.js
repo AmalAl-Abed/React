d831cc9528e4dc5c6df0f6ff575bae28fb5e60c5
AmalAl-Abed <amalabed116@gmail.com>
2022-06-21 14:10:13 +0300

    main

---
+++ 21-6-2022/task1/src/App.js
@@ -0,0 +1,14 @@
+
+import Nav from './components/nav';
+import Body from './components/body';
+function App(){
+
+  return(<div className='container'>
+
+<Nav />
+<Body />
+    
+  </div>)
+
+}
+export default App;
---
+++ 21-6-2022/task1/src/components/body.js
@@ -0,0 +1,41 @@
+function Body(){
+
+    return( <div className="container">
+    
+    <h1>Title:Welcome</h1>
+    <p style={{color:'blue'}}>This Is paragraph This Is paragraph This Is paragraph This Is paragraph</p>
+    <ul>
+<form>
+        <li>
+            
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"></img>
+           
+                 <input type='checkbox'/>
+                 <label> Html</label>
+        
+        </li>
+        <li>
+            
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"></img>
+           
+                 <input type='checkbox'/>
+                 <label> Html</label>
+        
+        </li>
+        <li>
+            
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"></img>
+           
+                 <input type='checkbox'/>
+                 <label> Html</label>
+        
+        </li>
+
+        </form>
+    </ul>
+    
+    </div>
+    )
+  
+  }
+  export default Body;
