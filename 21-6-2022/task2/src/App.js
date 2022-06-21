afde5dd5af15d8204034803d102a75c851f84e8c
AmalAl-Abed <amalabed116@gmail.com>
2022-06-22 00:22:00 +0300

    main

---
+++ 21-6-2022/task2/src/App.js
@@ -0,0 +1,16 @@
+
+
+import Nav from './components/nav';
+import Card from './components/card';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Nav />
+     <Card />
+    </div>
+  );
+}
+
+export default App;
